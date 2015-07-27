0c466ee0cd4999abd21974f712aff7733622f9f3
Heejong Ahn <heejongahn@gmail.com>
2015-07-27 13:28:13 +0900

    miscs
    1. template / view function renaming
    2. Little layout change (<br> added below each post title)
    3. Now posts are shown in desc order

--- hjlog/views.py
+++ hjlog/views.py
@@ -1,5 +1,6 @@
 from hjlog import app, db
 from flask import render_template, redirect, request, url_for
+from sqlalchemy import desc
 from .models import Post
 from .forms import PostForm
 
@@ -9,15 +10,15 @@ def index():
     return render_template('index.html')
 
 # Blog
-@app.route('/post')
-def post():
-    posts = Post.query.all()
-    return render_template('post.html', posts = posts)
+@app.route('/posts')
+def posts():
+    posts = Post.query.order_by(desc(Post.datetime)).all()
+    return render_template('posts.html', posts = posts)
 
 @app.route('/post/<id>')
-def article(id):
+def post(id):
     post = Post.query.filter_by(id=id).one()
-    return render_template('article.html', post=post)
+    return render_template('post.html', post=post)
 
 @app.route('/write', methods=['GET', 'POST'])
 def write():
@@ -28,6 +29,6 @@ def write():
 
         db.session.add(post)
         db.session.commit()
-        return redirect(url_for('article', id=post.id))
+        return redirect(url_for('posts', id=post.id))
 
     return render_template('write.html', form=form)
