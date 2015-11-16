e949a898750dd7fac8e1a4079512ae7a28da6b4c
Heejong Ahn <heejongahn@gmail.com>
2015-11-16 18:07:56 +0900

    README update + removed useless lines

--- dev_run.py
+++ dev_run.py
@@ -3,8 +3,4 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 if __name__ == '__main__':
-    os.environ['DATABASE_URL'] = (
-            'sqlite:///' + os.path.join(basedir, 'hjlog.db') +
-            '?check_same_thread=False')
-
     app.run(host='0.0.0.0', debug=True)
