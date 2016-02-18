a1aad2d152c215335eae5ac5f2c59212f71c2372
Heejong Ahn <heejongahn@gmail.com>
2016-02-18 22:42:39 +0900

    rollover bug-causing google analytics script

--- hjlog/static/js/hjlog.js
+++ hjlog/static/js/hjlog.js
@@ -28,11 +28,3 @@ var navToggle = function() {
     nav.style.display = 'block';
   }
 }
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-73895875-1', 'auto');
-ga('send', 'pageview');
