2c1b52d4bd908e92e858e78ad6f12ea6070a3e6d
Heejong Ahn <heejongahn@gmail.com>
2016-02-21 18:08:21 +0900

    js/ 4 spaces -> 2 spaces

--- hjlog/static/js/photoAjax.js
+++ hjlog/static/js/photoAjax.js
@@ -1,104 +1,104 @@
 function dataURItoBlob(dataURI) {
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+    byteString = atob(dataURI.split(',')[1]);
+  else
+    byteString = unescape(dataURI.split(',')[1]);
 
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-    var ab = new ArrayBuffer(byteString.length);
-    var ia = new Uint8Array(ab);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-    var bb = new Blob([ab], {"type": mimeString});
-    return bb;
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  var bb = new Blob([ab], {"type": mimeString});
+  return bb;
 }
 
 
 $(document).ready(function() {
   $(function() {
-    var csrftoken = $('meta[name=csrf-token]').attr('content')
+  var csrftoken = $('meta[name=csrf-token]').attr('content')
 
-    $.ajaxSetup({
-      beforeSend: function(xhr, settings) {
-        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
-          xhr.setRequestHeader("X-CSRFToken", csrftoken)
+  $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+    if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+      xhr.setRequestHeader("X-CSRFToken", csrftoken)
+    }
+    }
+  });
+
+  $('#photoupload').change(function() {
+    var file = $('#photoupload')[0].files[0];
+    var filename = file.name;
+    console.log(filename);
+    var img = document.createElement('img');
+    var reader = new FileReader();
+    var dataURL = null;
+
+    reader.onload = function(e)
+    {
+      img.src = e.target.result;
+
+      img.onload = function () {
+        console.log("image load");
+
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+
+        var MAX_LENGTH = 800;
+        var width = img.width;
+        var height = img.height;
+
+        if (height > width) {
+          if (MAX_LENGTH < height) {
+            width *= MAX_LENGTH/height;
+            height = MAX_LENGTH;
+          }
+        } else {
+          if (MAX_LENGTH < width) {
+            height *= MAX_LENGTH/width;
+            width = MAX_LENGTH;
+          }
         }
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext('2d')
+        ctx.drawImage(img, 0, 0, width, height);
+
+        dataURL = canvas.toDataURL('image/jpeg');
+        var blob = dataURItoBlob(dataURL);
+        var formData = new FormData();
+
+        formData.append('file', blob, filename);
+
+        $.ajax({
+        type: 'POST',
+        url: '/photoajax',
+        data: formData,
+        enctype: "multipart/form-data",
+        contentType: false,
+        processData: false,
+        dataType: 'json',
+        success: function(data, textStatus, jqXHR) {
+          if (data['correct']) {
+            $("textarea").val($("textarea").val() + '![' + data['name'] +
+              ']('+data['url']+')');
+            $("input[name=photonames]").val(function(i, v) {
+              return v + " " + data['name'];
+            });
+          } else {
+            alert("파일의 형식이 올바르지 않아요!")
+          }
+         },
+        error: function(xhr) {
+          console.log(xhr);
+        }
+        });
       }
-    });
-
-    $('#photoupload').change(function() {
-        var file = $('#photoupload')[0].files[0];
-        var filename = file.name;
-        console.log(filename);
-        var img = document.createElement('img');
-        var reader = new FileReader();
-        var dataURL = null;
-
-        reader.onload = function(e)
-        {
-            img.src = e.target.result;
-
-            img.onload = function () {
-                console.log("image load");
-
-                var canvas = document.createElement('canvas');
-                var ctx = canvas.getContext('2d');
-                ctx.drawImage(img, 0, 0);
-
-                var MAX_LENGTH = 800;
-                var width = img.width;
-                var height = img.height;
-
-                if (height > width) {
-                    if (MAX_LENGTH < height) {
-                        width *= MAX_LENGTH/height;
-                        height = MAX_LENGTH;
-                    }
-                } else {
-                    if (MAX_LENGTH < width) {
-                        height *= MAX_LENGTH/width;
-                        width = MAX_LENGTH;
-                    }
-                }
-                canvas.width = width;
-                canvas.height = height;
-                var ctx = canvas.getContext('2d')
-                ctx.drawImage(img, 0, 0, width, height);
-
-                dataURL = canvas.toDataURL('image/jpeg');
-                var blob = dataURItoBlob(dataURL);
-                var formData = new FormData();
-
-                formData.append('file', blob, filename);
-
-                $.ajax({
-                type: 'POST',
-                url: '/photoajax',
-                data: formData,
-                enctype: "multipart/form-data",
-                contentType: false,
-                processData: false,
-                dataType: 'json',
-                success: function(data, textStatus, jqXHR) {
-                    if (data['correct']) {
-                        $("textarea").val($("textarea").val() + '![' + data['name'] +
-                            ']('+data['url']+')');
-                        $("input[name=photonames]").val(function(i, v) {
-                            return v + " " + data['name'];
-                        });
-                    } else {
-                        alert("파일의 형식이 올바르지 않아요!")
-                    }
-                 },
-                error: function(xhr) {
-                    console.log(xhr);
-                }
-                });
-            }
-        }
-        reader.readAsDataURL(file);
-     });
+    }
+    reader.readAsDataURL(file);
+   });
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -8,39 +8,39 @@ var rootAssetPath = './hjlog/static';
 var absRootAssetPath = path.resolve(rootAssetPath);
 
 var plugins = [
-    new ManifestRevisionPlugin(path.join('.', 'manifest.json'), {
-        rootAssetPath: rootAssetPath,
-        ignorePaths: [/.DS_Store$/, '/css', '/js', '/build']
-    })
+  new ManifestRevisionPlugin(path.join('.', 'manifest.json'), {
+    rootAssetPath: rootAssetPath,
+    ignorePaths: [/.DS_Store$/, '/css', '/js', '/build']
+  })
 ];
 
 if (process.env.WEBPACK === 'release') {
-    plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }));
+  plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }));
 }
 
 module.exports = {
-    entry: {
-        app_js: rootAssetPath + '/js/hjlog.js'
-    },
-    output: {
-        path: 'hjlog/static/build',
-        publicPath: '/static/build/',
-        filename: '[name].[hash].js',
-    },
-    resolve: {
-        root: absRootAssetPath + '/js',
-        extensions: ['', '.js', '.css']
-    },
-    module: {
+  entry: {
+    app_js: rootAssetPath + '/js/hjlog.js'
+  },
+  output: {
+    path: 'hjlog/static/build',
+    publicPath: '/static/build/',
+    filename: '[name].[hash].js',
+  },
+  resolve: {
+    root: absRootAssetPath + '/js',
+    extensions: ['', '.js', '.css']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(jpe?g|png|gif|svg([\?]?.*))$/i,
         loaders: [
-            {
-                test: /\.(jpe?g|png|gif|svg([\?]?.*))$/i,
-                loaders: [
-                    'file?context=' + rootAssetPath + '&name=[path][name].[hash].[ext]',
-                    'image?bypassOnDebug&optimizationLevel=7&interlaced=false'
-                ]
-            }
+          'file?context=' + rootAssetPath + '&name=[path][name].[hash].[ext]',
+          'image?bypassOnDebug&optimizationLevel=7&interlaced=false'
         ]
-    },
-    plugins: plugins
+      }
+    ]
+  },
+  plugins: plugins
 };
