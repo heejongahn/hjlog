2cda27d619d563fcdf6c9eee397c3333f8e2dee9
Heejong Ahn <heejongahn@gmail.com>
2016-02-25 23:15:29 +0900

    Refactored photoAjax.js
    1. 필요없는 $(function() {
        ...
        }); 삭제
    
    2. 가능하면 jQuery 대신 pure javascript API 사용하기 (성능 문제)
    
    3. ab, ia 대신 좀 길어도 의미가 더 명확한 변수명 사용
    
    4. 인덴테이션 지키기
    
    5. 20줄이 넘어가면 가능하면 하나의 기능을 하는 함수로 빼기

--- hjlog/static/js/photoAjax.js
+++ hjlog/static/js/photoAjax.js
@@ -1,104 +1,111 @@
-function dataURItoBlob(dataURI) {
-  var byteString;
-  if (dataURI.split(',')[0].indexOf('base64') >= 0)
-    byteString = atob(dataURI.split(',')[1]);
-  else
-    byteString = unescape(dataURI.split(',')[1]);
-
-  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-  var ab = new ArrayBuffer(byteString.length);
-  var ia = new Uint8Array(ab);
-  for (var i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-  var bb = new Blob([ab], {"type": mimeString});
-  return bb;
-}
-
-
 $(document).ready(function() {
-  $(function() {
   var csrftoken = $('meta[name=csrf-token]').attr('content')
 
   $.ajaxSetup({
     beforeSend: function(xhr, settings) {
-    if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
-      xhr.setRequestHeader("X-CSRFToken", csrftoken)
-    }
+      if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken)
+      }
     }
   });
 
   $('#photoupload').change(function() {
     var file = $('#photoupload')[0].files[0];
-    var filename = file.name;
-    console.log(filename);
     var img = document.createElement('img');
     var reader = new FileReader();
-    var dataURL = null;
 
-    reader.onload = function(e)
-    {
+    reader.onload = function(e) {
       img.src = e.target.result;
-
       img.onload = function () {
-        console.log("image load");
-
-        var canvas = document.createElement('canvas');
-        var ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0);
-
-        var MAX_LENGTH = 800;
-        var width = img.width;
-        var height = img.height;
-
-        if (height > width) {
-          if (MAX_LENGTH < height) {
-            width *= MAX_LENGTH/height;
-            height = MAX_LENGTH;
-          }
-        } else {
-          if (MAX_LENGTH < width) {
-            height *= MAX_LENGTH/width;
-            width = MAX_LENGTH;
-          }
-        }
-        canvas.width = width;
-        canvas.height = height;
-        var ctx = canvas.getContext('2d')
-        ctx.drawImage(img, 0, 0, width, height);
-
-        dataURL = canvas.toDataURL('image/jpeg');
+        var canvas = resize(img);
+        var dataURL = canvas.toDataURL('image/jpeg');
         var blob = dataURItoBlob(dataURL);
         var formData = new FormData();
 
-        formData.append('file', blob, filename);
-
-        $.ajax({
-        type: 'POST',
-        url: '/photoajax',
-        data: formData,
-        enctype: "multipart/form-data",
-        contentType: false,
-        processData: false,
-        dataType: 'json',
-        success: function(data, textStatus, jqXHR) {
-          if (data['correct']) {
-            $("textarea").val($("textarea").val() + '![' + data['name'] +
-              ']('+data['url']+')');
-            $("input[name=photonames]").val(function(i, v) {
-              return v + " " + data['name'];
-            });
-          } else {
-            alert("파일의 형식이 올바르지 않아요!")
-          }
-         },
-        error: function(xhr) {
-          console.log(xhr);
-        }
-        });
+        formData.append('file', blob, file.name);
+        upload(formData);
       }
     }
+
     reader.readAsDataURL(file);
-   });
   });
 });
+
+function resize(img) {
+  var canvas = document.createElement('canvas');
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0);
+
+  var MAX_LENGTH = 800;
+  var width = img.width;
+  var height = img.height;
+
+  if (height > width) {
+    if (MAX_LENGTH < height) {
+      width *= MAX_LENGTH/height;
+      height = MAX_LENGTH;
+    }
+  } else {
+    if (MAX_LENGTH < width) {
+      height *= MAX_LENGTH/width;
+      width = MAX_LENGTH;
+    }
+  }
+
+  canvas.width = width;
+  canvas.height = height;
+  ctx = canvas.getContext('2d')
+  ctx.drawImage(img, 0, 0, width, height);
+
+  return canvas;
+}
+
+function dataURItoBlob(dataURI) {
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+    byteString = atob(dataURI.split(',')[1]);
+  } else {
+    byteString = unescape(dataURI.split(',')[1]);
+  }
+
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+  var arrayBuffer = new ArrayBuffer(byteString.length);
+  var intArray = new Uint8Array(arrayBuffer);
+
+  for (var i = 0; i < byteString.length; i++) {
+    intArray[i] = byteString.charCodeAt(i);
+  }
+
+  var blob = new Blob([arrayBuffer], {"type": mimeString});
+  return blob;
+}
+
+function upload(formData) {
+  $.ajax({
+    type: 'POST',
+    url: '/photoajax',
+    data: formData,
+    enctype: "multipart/form-data",
+    contentType: false,
+    processData: false,
+    dataType: 'json',
+    success: function(data, textStatus, jqXHR) {
+      if (data['correct']) {
+        updateForm(data['name'], data['url']);
+      } else {
+        alert("파일의 형식이 올바르지 않아요!")
+      }
+    },
+    error: function(xhr) {
+      console.log(xhr);
+    }
+  });
+}
+
+function updateForm(name, url) {
+  var textarea = document.getElementsByTagName('textarea')[0];
+  var photoNames = document.getElementsByName('photonames')[0];
+
+  textarea.innerHTML += ('![' + name + '](' + url + ')');
+  photoNames.innerHTML += (" " + name);
+}
