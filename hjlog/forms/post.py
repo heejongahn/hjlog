a50b4b1a51f1e38342ab74c30f7df1e1d9ae617a
Heejong Ahn <heejongahn@gmail.com>
2016-03-08 14:43:22 +0100

    Category break down!

--- hjlog/forms/post.py
+++ hjlog/forms/post.py
@@ -6,4 +6,5 @@ class PostForm(Form):
     title = StringField('제목', validators=[InputRequired(), Length(max=120)])
     body = TextAreaField('내용', validators=[InputRequired()])
     tags = StringField('글갈피', validators=[Length(max=100)])
-    category = SelectField('분류', choices=[('daily', 'Daily'), ('study', 'Study')])
+    category = SelectField('분류', choices=[('everyday', 'EVERYDAY'),
+        ('study', 'STUDY'), ('idea', 'IDEA'), ('world', 'WORLD')])
