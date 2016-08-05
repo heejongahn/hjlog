7a8b6bb5eb5b83f8f975a93f5e401ee99e38be41
Hajin Shim <shimazing@kaist.ac.kr>
2016-08-05 14:06:43 +0200

    Redirect to previous page by using next arg

--- hjlog/forms/login.py
+++ hjlog/forms/login.py
@@ -1,9 +1,8 @@
 from flask_wtf import Form
 from wtforms import TextField, PasswordField, HiddenField
 from wtforms.validators import InputRequired
-from urllib.parse import urlparse, urljoin
-from flask import request, url_for, redirect
-
+from urllib.parse import urlparse, urljoin, unquote
+from flask import request, url_for, redirect, session
 
 
 class RedirectForm(Form):
@@ -12,7 +11,7 @@ class RedirectForm(Form):
     def __init__(self, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
         if not self.next.data:
-            self.next.data = get_redirect_target() or ''
+            self.next.data = get_redirect_target()
 
     def redirect(self, endpoint='about', **values):
         if is_safe_url(self.next.data):
@@ -26,9 +25,9 @@ class LoginForm(RedirectForm):
     password = PasswordField('Password', validators=[InputRequired()])
 
 
-###################
-# helper function #
-###################
+####################
+# helper functions #
+####################
 
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
@@ -37,10 +36,8 @@ def is_safe_url(target):
             ref_url.netloc == test_url.netloc
 
 def get_redirect_target():
-    for target in request.args.get('next'), request.referrer:
+    for target in request.args.get("next"), request.referrer:
         if not target:
             continue
         if is_safe_url(target):
-            return target
-
-
+            return unquote(target or '')
--- hjlog/views/login.py
+++ hjlog/views/login.py
@@ -1,8 +1,9 @@
 from hjlog import lm
-from flask import render_template, redirect, flash, url_for, request
+from flask import render_template, redirect, flash, url_for, request, session
 from hjlog.models import User
 from hjlog.forms import LoginForm
 from flask.ext.login import login_user, logout_user, current_user, login_required
+from urllib.parse import urlparse
 
 msg_invalid_user = '등록된 관리자가 아닙니다 :('
 msg_login_success = '관리자님 환영합니다 :)'
@@ -14,6 +15,9 @@ def register(app):
         if current_user.is_authenticated():
             return redirect(url_for('about'))
 
+        if 'next_url' not in session:
+            session['next_url'] = urlparse(request.referrer).path
+
         form = LoginForm()
         if request.method == 'POST' and form.validate_on_submit():
             user = User.query.filter_by(username=form.username.data).first()
@@ -21,10 +25,12 @@ def register(app):
             if user and user.is_correct_password(form.password.data):
                 login_user(user)
                 flash(msg_login_success, 'success')
-                return redirect(url_for('about'))
+                session.pop('next_url', None)
+                return form.redirect(url_for('about'))
             else:
                 flash(msg_invalid_user, 'error')
-                return redirect(url_for('login'))
+                return redirect(url_for('login', next=session['next_url']))
+
 
         return render_template('login.html', form=form)
 
@@ -34,7 +40,6 @@ def register(app):
         form = LoginForm()
         logout_user()
         flash(msg_logout_success, 'success')
-
         return form.redirect(url_for('about'))
 
     ####################
