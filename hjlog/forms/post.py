357fc83908fe09da2c69be78afdae9f1cf5c4b0d
Heejong Ahn <heejongahn@gmail.com>
2016-08-09 12:43:58 +0200

    Change label text of 'private' Field

--- hjlog/forms/post.py
+++ hjlog/forms/post.py
@@ -5,7 +5,7 @@ from wtforms.validators import InputRequired, Optional, Length
 class PostForm(Form):
     title = StringField('제목', validators=[InputRequired(), Length(max=120)])
     body = TextAreaField('내용', validators=[InputRequired()])
-    private = BooleanField('이 글을 비공개로 작성합니다', default=True)
+    private = BooleanField('비공개 설정', default=True)
     tags = StringField('글갈피', validators=[Length(max=100)])
     category = SelectField('분류', validators=[Optional()],
             choices=[('everyday', 'EVERYDAY'),
