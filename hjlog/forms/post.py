2b02a5318e6519fbcdafc953847e9ee09deddaa1
Heejong Ahn <heejongahn@gmail.com>
2016-08-09 08:51:55 +0200

    Add 'private' field @ PostForm and template for it

--- hjlog/forms/post.py
+++ hjlog/forms/post.py
@@ -1,10 +1,11 @@
 from flask_wtf import Form
-from wtforms import TextAreaField, StringField, SelectField
+from wtforms import TextAreaField, StringField, SelectField, BooleanField
 from wtforms.validators import InputRequired, Optional, Length
 
 class PostForm(Form):
     title = StringField('제목', validators=[InputRequired(), Length(max=120)])
     body = TextAreaField('내용', validators=[InputRequired()])
+    private = BooleanField('이 글을 비공개로 작성합니다', default=True)
     tags = StringField('글갈피', validators=[Length(max=100)])
     category = SelectField('분류', validators=[Optional()],
             choices=[('everyday', 'EVERYDAY'),
