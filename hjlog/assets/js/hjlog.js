eb5f56977008184e5cd1c0d576650851d869af29
Heejong Ahn <heejongahn@gmail.com>
2016-08-14 21:17:26 +0900

    Add Google Analytics

--- hjlog/assets/js/hjlog.js
+++ hjlog/assets/js/hjlog.js
@@ -48,3 +48,11 @@ const photoUploadForm = document.getElementById("photoupload");
 if (photoUploadForm) {
   photoUploadForm.addEventListener("change", asyncPhotoUpload);
 }
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-73895875-1', 'auto');
+ga('send', 'pageview');
